b1f1697690032da3e56276ac95b5cf55c4df29b0
slerR <nazarikovslava@gmail.com>
2022-10-06 23:35:04 +0300

    feat: I have done the program, which write your name

---
+++ 03_cpp/intro/intro.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+int main()
+{
+    using namespace std;
+    string name;
+    cout << "Write your name here, please" << endl;
+    cin >> name;
+    cout << "Hello, "<< name << endl;
+   system("Pause");
+    return 0 ; 
+}
