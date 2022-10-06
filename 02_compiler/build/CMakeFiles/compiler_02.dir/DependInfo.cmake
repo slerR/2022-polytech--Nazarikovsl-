c7b463dfffa768f94f2c99a5022d00860055ad8e
slerR <nazarikovslava@gmail.com>
2022-10-06 21:33:29 +0300

    feat: compare different ways of the compilation

---
+++ 02_compiler/build/CMakeFiles/compiler_02.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/User/Documents/GitHub/2022-polytech--Nazarikov-/02_compiler/func.cpp" "CMakeFiles/compiler_02.dir/func.cpp.obj" "gcc" "CMakeFiles/compiler_02.dir/func.cpp.obj.d"
+  "C:/Users/User/Documents/GitHub/2022-polytech--Nazarikov-/02_compiler/hello_world.cpp" "CMakeFiles/compiler_02.dir/hello_world.cpp.obj" "gcc" "CMakeFiles/compiler_02.dir/hello_world.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 02_compiler/build/CMakeFiles/compiler_02.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/compiler_02.dir/func.cpp.obj"
+  "CMakeFiles/compiler_02.dir/func.cpp.obj.d"
+  "CMakeFiles/compiler_02.dir/hello_world.cpp.obj"
+  "CMakeFiles/compiler_02.dir/hello_world.cpp.obj.d"
+  "compiler_02.exe"
+  "compiler_02.exe.manifest"
+  "compiler_02.pdb"
+  "libcompiler_02.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/compiler_02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
