b30d1eac38d5230a76dccd54a78ec39a04ca53f9
slerR <nazarikovslava@gmail.com>
2022-11-11 12:47:53 +0300

    fix: do some changes

---
+++ 04_search_sort/sort_cmp/build/CMakeFiles/sort_cmp.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/User/Documents/GitHub/2022-polytech--Nazarikov-/04_search_sort/sort_cmp/search_time.cpp" "CMakeFiles/sort_cmp.dir/search_time.cpp.obj" "gcc" "CMakeFiles/sort_cmp.dir/search_time.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 04_search_sort/sort_cmp/build/CMakeFiles/sort_cmp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sort_cmp.dir/search_time.cpp.obj"
+  "CMakeFiles/sort_cmp.dir/search_time.cpp.obj.d"
+  "libsort_cmp.dll.a"
+  "sort_cmp.exe"
+  "sort_cmp.exe.manifest"
+  "sort_cmp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sort_cmp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
