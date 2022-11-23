4f9755c072db2ba5afd0d6c2fda5c48db4c75a31
slerR <nazarikovslava@gmail.com>
2022-11-23 23:00:16 +0300

    feat: did warm-up for 06_pointers_references

---
+++ 06_pointers_references/intro/intro.cpp
@@ -0,0 +1,28 @@
+#include <cassert>
+using namespace std;
+
+unsigned int length(const char* str)
+{
+int length = 0;
+
+const char* parr = str;
+
+for(int i = 0; parr[i] != '\0'; i++ ){
+    length++;
+}
+
+return length;
+
+delete[] parr;
+
+}
+
+int main()
+{
+    assert(length("a") == 1);
+    assert(length("ab") == 2);
+    assert(length("") == 0);
+    assert(length("hello world") == 11);
+    assert(length(nullptr) == 0);
+    return 0;
+}
