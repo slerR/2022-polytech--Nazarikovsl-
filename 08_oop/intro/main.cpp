15cf0adb6726703039dbc471ce5b600ca7cfbd8a
slerR <nazarikovslava@gmail.com>
2022-12-09 12:03:50 +0300

    fix: did some changes

--- 08_oop/intro/main.cpp
+++ 08_oop/intro/main.cpp
@@ -3,7 +3,7 @@
 
 int main() {
   String biba = "ABO";
-  String boba = biba + " BA";
+  String boba = biba + "BA";
   std::cout << boba << '\n' << boba.get_length();
   system("pause 0");
   return 0;
